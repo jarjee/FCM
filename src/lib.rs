0aaeeff8ff7e98bd4806ec38a060b9a9b6e02fab
Nathan Smyth <nathan.andrew.smyth@gmail.com>
2017-05-25 21:39:44 +1000

    Giving test a more reasonable name

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod types;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn a_new_house_has_no_demand() {
         let t = ::types::House::new(1);
         assert!(!t.demand.hasDemand());
     }
