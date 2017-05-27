c06263909073ac578fd8f664c1df8a6ec013c909
Nathan Smyth <nathan.andrew.smyth@gmail.com>
2017-05-27 19:44:45 +1000

    Adding helper functions for adjusting demand

--- src/types.rs
+++ src/types.rs
@@ -11,24 +11,56 @@ impl Demand {
         return (self.burgers + self.pizzas + self.beers + self.sodas + self.lemonade) > 0;
     }
 
-    pub fn setBurgers(&mut self, burgs: u8) {
-        self.burgers = burgs;
+    pub fn demandArray(&self) -> [u8; 5] {
+        return [self.burgers(),
+                self.pizzas(),
+                self.beers(),
+                self.sodas(),
+                self.lemonade()];
+    }
+
+    pub fn setDemandArray(&mut self, demandArr: [u8; 5]) {
+        self.burgers = demandArr[0];
+        self.pizzas = demandArr[1];
+        self.beers = demandArr[2];
+        self.sodas = demandArr[3];
+        self.lemonade = demandArr[4];
+    }
+
+    pub fn setBurgers(&mut self, amount: u8) {
+        self.burgers = amount;
     }
 
     pub fn burgers(&self) -> u8 {
         return self.burgers;
     }
 
+    pub fn setPizzas(&mut self, amount: u8) {
+        self.pizzas = amount;
+    }
+
     pub fn pizzas(&self) -> u8 {
         return self.pizzas;
+    }
+
+    pub fn setBeers(&mut self, amount: u8) {
+        self.beers = amount;
     }
 
     pub fn beers(&self) -> u8 {
         return self.beers;
     }
 
+    pub fn setSodas(&mut self, amount: u8) {
+        self.sodas = amount;
+    }
+
     pub fn sodas(&self) -> u8 {
         return self.sodas;
+    }
+
+    pub fn setLemonade(&mut self, amount: u8) {
+        self.lemonade = amount;
     }
 
     pub fn lemonade(&self) -> u8 {
